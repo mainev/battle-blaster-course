e46e30ca60b6cb05b37d4e155ea7e0a5043acee3
mainev <mainevillarias@gmail.com>
2026-02-06 02:07:24 +0800

    created blueprint BP_Tank and set up base mesh, spring arm, etc.

--- Source/BattleBlaster/BasePawn.cpp
+++ Source/BattleBlaster/BasePawn.cpp
@@ -9,15 +9,16 @@ ABasePawn::ABasePawn()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-
+	// set the root component
 	CapsuleComp =  CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComp"));
 	SetRootComponent(CapsuleComp);
+
 
 	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
 	BaseMesh->SetupAttachment(CapsuleComp);
 
 	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TurretMesh"));
-	BaseMesh->SetupAttachment(TurretMesh);
+	TurretMesh->SetupAttachment(BaseMesh);
 
 }
 
