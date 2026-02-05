a239e6b79e7d9cfa7ff1f0cdd586a918b0b8b03a
mainev <mainevillarias@gmail.com>
2026-02-06 01:46:24 +0800

    added Tank class inheriting BasePawn

--- Source/BattleBlaster/BasePawn.h
+++ Source/BattleBlaster/BasePawn.h
@@ -16,16 +16,6 @@ public:
 	// Sets default values for this pawn's properties
 	ABasePawn();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 	UPROPERTY(VisibleAnywhere)
 	UCapsuleComponent* CapsuleComp;
