2d95aeee43c599fd50f5909f535e9e915492a5e9
mainev <mainevillarias@gmail.com>
2026-02-12 02:02:01 +0800

    added enemy towers

---
+++ Source/BattleBlaster/Tower.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+
+#include "Tank.h"
+#include "Tower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEBLASTER_API ATower : public ABasePawn
+{
+	GENERATED_BODY()
+
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	ATank* Tank;
+	
+};
